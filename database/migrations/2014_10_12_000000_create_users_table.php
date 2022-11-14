545f05c958f589c369bce1b6eef04d709faa58f6
AhmedFouad380 <36769581+AhmedFouad380@users.noreply.github.com>
2022-11-14 13:18:09 +0200

    A

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,6 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('is_active',['active','inactive'])->default('inactive');
-
             $table->rememberToken();
             $table->timestamps();
         });
