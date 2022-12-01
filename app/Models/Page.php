bc2699b5134e06b4c9dc01c23a3ea0310646e5eb
AhmedFouad380 <36769581+AhmedFouad380@users.noreply.github.com>
2022-12-01 22:30:28 +0200

    A

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -9,39 +9,39 @@ use Illuminate\Support\Facades\App;
 class Page extends Model
 {
     use HasFactory;
-    protected $appends = ['title','description'];
+        protected $appends = ['title','description'];
 
 
-    public function getDescriptionAttribute()
-    {
-        if ($locale = App::currentLocale() == "ar") {
-            return $this->ar_description;
-        } else {
-            return $this->en_description;
+        public function getDescriptionAttribute()
+        {
+            if ($locale = App::currentLocale() == "ar") {
+                return $this->ar_description;
+            } else {
+                return $this->en_description;
+            }
         }
-    }
-    public function getTitleAttribute()
-    {
-        if ($locale = App::currentLocale() == "ar") {
-            return $this->ar_title;
-        } else {
-            return $this->en_title;
+        public function getTitleAttribute()
+        {
+            if ($locale = App::currentLocale() == "ar") {
+                return $this->ar_title;
+            } else {
+                return $this->en_title;
+            }
         }
-    }
-    public function getImageAttribute($image)
-    {
-        if (!empty($image)) {
-            return asset('uploads/Page') . '/' . $image;
-        }
-        return null;
+        public function getImageAttribute($image)
+        {
+            if (!empty($image)) {
+                return asset('uploads/Page') . '/' . $image;
+            }
+            return null;
 
-    }
-
-    public function setImageAttribute($image)
-    {
-        if (is_file($image)) {
-            $imageFields = upload($image, 'Page');
-            $this->attributes['image'] = $imageFields;
         }
-    }
+
+        public function setImageAttribute($image)
+        {
+            if (is_file($image)) {
+                $imageFields = upload($image, 'Page');
+                $this->attributes['image'] = $imageFields;
+            }
+        }
 }
